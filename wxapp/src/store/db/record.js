b7939b955276f74d5140a5c4d870209dae2d1042
YuanGuangsheng <yuanguangsheng@gmail.com>
2019-08-25 14:17:59 +0200

    fixed other Record list

--- wxapp/src/store/db/record.js
+++ wxapp/src/store/db/record.js
@@ -1,9 +1,9 @@
-import { GetCollection } from './base'
+import { GetCollection, CMD } from './base'
 import { LoginUser } from './user'
 
 export function NewRecord () {
   return {
-    userId: "", // 用户ID
+    user: null, // 用户信息
     recordDatetime: "", // 打卡时间
     answerRight: "", // 答对题数
     answerDuration: "", // 答题时间
@@ -11,11 +11,18 @@ export function NewRecord () {
     questionTotal: "", // 总题数
     questionIdList: "", // 问题id列表
     answerList: "", // 答案列表
-    clockInDeadline: "" // 打卡Deadline
+    clockInDeadline: "", // 打卡Deadline
   }
 }
 
 export function CreateRecord(record) {
+  record.user = {
+    userId: LoginUser.userId,
+    userName: LoginUser.userName,
+    headPortrait: LoginUser.headPortrait,
+    clockInDay: LoginUser.clockInDay
+  }
+
   return new Promise((resolve, reject) => {
     let recordCol = GetCollection('record')
     recordCol.add({
@@ -40,8 +47,22 @@ export function GetUserRecord(nowDate) {
   return new Promise((resolve, reject) => {
     let recordCol = GetCollection('record')
     recordCol.where({
-      userId: LoginUser.userId,
+      'user.userId': LoginUser.userId,
       clockInDeadline: nowDate
+    }).get({
+      success: resolve,
+      fail: reject
+    })
+  })
+}
+
+export function GetOtherRecord(deadline) {
+  return new Promise((resolve, reject) => {
+    let recordCol = GetCollection('record')
+    console.log(LoginUser.userId)
+    recordCol.where({
+      clockInDeadline: deadline,
+      'user.userId': CMD.neq(LoginUser.userId)
     }).get({
       success: resolve,
       fail: reject
